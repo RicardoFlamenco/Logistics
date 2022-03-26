a0184c62380e04ca683aa1a44135961c47e9aca3
Ricardo Montenegro <rflamencom@outlook.com>
2022-03-26 15:15:47 -0600

    setting endpoints

--- src/main/resources/db/migration/V1_2_0__initializer_data.sql
+++ src/main/resources/db/migration/V1_2_0__initializer_data.sql
@@ -1,4 +1,4 @@
-insert into logistics.roles (name) values ('ROLE_ADMIN');
 insert into logistics.roles (name) values ('ROLE_USER');
+insert into logistics.roles (name) values ('ROLE_ADMIN');
 insert into logistics.roles (name) values ('ROLE_MODERATOR');
 insert into logistics.roles (name) values ('ROLE_CLIENTE');
