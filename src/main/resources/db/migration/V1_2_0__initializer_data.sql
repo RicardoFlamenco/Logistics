f569936cbf32f628fa8d9c1611dea91ec047bffa
Ricardo Montenegro <rflamencom@outlook.com>
2022-03-26 13:30:17 -0600

    insert data init for deploy 3, fix

--- src/main/resources/db/migration/V1_2_0__initializer_data.sql
+++ src/main/resources/db/migration/V1_2_0__initializer_data.sql
@@ -2,4 +2,3 @@ insert into logistics.roles (name) values ('ROLE_ADMIN');
 insert into logistics.roles (name) values ('ROLE_USER');
 insert into logistics.roles (name) values ('ROLE_MODERATOR');
 insert into logistics.roles (name) values ('ROLE_CLIENTE');
-INSERT INTO logistics.users (id, email, password, username) VALUES (0, 'fmricardo86@gmail.com', 'admin', 'admin');
